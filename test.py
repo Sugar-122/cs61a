addb15644871813199773f292b02406b9a50da45
Sugar-122 <2894981413@qq.com>
2023-03-24 15:06:33 +0800

    完成了Disc04

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+# Disc 03
 def multiply(m, n):
     """ Takes two positive integers and returns their product using recursion.
     >>> multiply(5, 3)
@@ -131,11 +132,11 @@ def count_k(n, k):
         return count
 
 
-#  print(multiply(5, 4))
-print(is_prime(2))
-print(is_prime(16))
-print(is_prime(521))
-#  print(count_stair_ways(4))
+# print(multiply(5, 4))
+# print(is_prime(2))
+# print(is_prime(16))
+# print(is_prime(521))
+# print(count_stair_ways(4))
 # print(merge(31, 42))
 # a = hailstone(10)
 # print(a)
@@ -145,3 +146,131 @@ print(is_prime(521))
 # print(count_k(300, 1))
 # print(count_k(3, 3))
 # print(count_k(4, 4))
+
+# Disc 04
+def my_map(fn, seq):
+    """Applies fn onto each element in seq and returns a list.
+    >>> my_map(lambda x: x*x, [1, 2, 3])
+    [1, 4, 9]
+    """
+    "*** YOUR CODE HERE ***"
+    ret = []
+    for elem in seq:
+        ret.append(fn(elem))
+    return ret
+
+
+# print(my_map(lambda x: x*x, [1, 2, 3]))
+
+def my_filter(pred, seq):
+    """Keeps elements in seq only if they satisfy pred.
+    >>> my_filter(lambda x: x % 2 == 0, [1, 2, 3, 4])  # new list has only even-valued elements
+    [2, 4]
+    """
+    "*** YOUR CODE HERE ***"
+    ret = []
+    for elem in seq:
+        if pred(elem):
+            ret.append(elem)
+        else:
+            continue
+    return ret
+
+
+# print(my_filter(lambda x: x % 2 == 0, [1, 2, 3, 4]))
+
+def my_reduce(combiner, seq):
+    """Combines elements in seq using combiner.
+    seq will have at least one element.
+    >>> my_reduce(lambda x, y: x + y, [1, 2, 3, 4])  # 1 + 2 + 3 + 4
+    10
+    >>> my_reduce(lambda x, y: x * y, [1, 2, 3, 4])  # 1 * 2 * 3 * 4
+    24
+    >>> my_reduce(lambda x, y: x * y, [4])
+    4
+    >>> my_reduce(lambda x, y: x + 2 * y, [1, 2, 3]) # (1 + 2 * 2) + 2 * 3
+    11
+    """
+    "*** YOUR CODE HERE ***"
+    result = seq[0]
+    for elem in seq[1:]:
+        result = combiner(result, elem)
+    return result
+
+
+# print(my_reduce(lambda x, y: x + y, [1, 2, 3, 4]))
+# print(my_reduce(lambda x, y: x * y, [1, 2, 3, 4]))
+# print(my_reduce(lambda x, y: x * y, [4]))
+# print(my_reduce(lambda x, y: x + 2 * y, [1, 2, 3]))
+
+def count_palindromes(L):
+    """The number of palindromic words in the sequence of strings
+    L (ignoring case).
+
+    >>> count_palindromes(("Acme", "Madam", "Pivot", "Pip"))
+    2
+    """
+    count = 0
+    for elem in L:
+        if elem.lower() == elem[::-1].lower():
+            count += 1
+    return count
+
+
+# print(count_palindromes(("Acme", "Madam", "Pivot", "Pip")))
+# print(count_palindromes('hhhhh'))
+
+def even_weighted(s):
+    """
+    >>> x = [1, 2, 3, 4, 5, 6]
+    >>> even_weighted(x)
+    [0, 6, 20]
+    """
+    ret = []
+    i = 0
+    for elem in s:
+        if i % 2 == 0:
+            ret.append(elem*i)
+        i += 1
+    return ret
+
+
+# print(even_weighted([1, 2, 3, 4, 5, 6]))
+
+def max_product(s):
+    """Return the maximum product that can be formed using
+    non-consecutive elements of s.
+    >>> max_product([10,3,1,9,2]) # 10 * 9
+    90
+    >>> max_product([5,10,5,10,5]) # 5 * 5 * 5
+    125
+    >>> max_product([])
+    1
+    """
+    product1 = 0
+    product2 = 0
+    ret = 0
+    if len(s) == 0:
+        return 1
+    elif len(s) == 1:
+        return s[0]
+    else:
+        max_seq_1 = max_product(s[2:])
+        max_seq_2 = max_product(s[3:])
+        if max_seq_1 > 1:
+            product1 = max_seq_1*s[0]
+        else:
+            product1 = s[0]
+        if max_seq_2 > 1:
+            product2 = max_seq_2*s[1]
+        else:
+            product2 = s[1]
+        ret = max(product1, product2)
+    return ret
+
+
+# print(max_product([10, 3, 1, 9, 2]))
+# print(max_product([5, 10, 5, 10, 5]))
+# print(max_product([]))
+
+
